3a068e24771cb57aa52829d342c8354f7c0a4633
Kthor426 <63168599+Kthor426@users.noreply.github.com>
2025-07-07 01:47:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -82,10 +82,10 @@ stopData.slice(0, 3).forEach((visit, index, array) => {
         const depM = depDate.getMinutes().toString().padStart(2, '0');
         details += `<br><em>Scheduled to depart terminal at ${depH}:${depM}</em>`;
       } else {
-        details += `<br><em>At Terminal</em>`;
+        details += `<br><em>At Terminal — Passed departure time</em>`;
       }
     } else {
-      details += `<br><em>At Terminal</em>`;
+      details += `<br><em>At Terminal — Passed departure time</em>`;
     }}
 
   details += `</p>`;

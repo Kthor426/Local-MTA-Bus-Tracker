ea2838b74f724fd0a32e14769b04188b27394700
Kthor426 <63168599+Kthor426@users.noreply.github.com>
2025-07-07 03:17:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = '9e82d6fc-7ab9-4f2e-a518-ffc37d9af15c';
-const stopIds = ['MTA_553332', 'MTA_550572', 'MTA_551974', 'MTA_551950'];
+const stopIds = ['MTA_550585', 'MTA_550572', 'MTA_551974', 'MTA_551950'];
 const stopNames = {
-  'MTA_553332': { name: 'Q18 – Astoria', direction: 'north' },
+  'MTA_550585': { name: 'Q18 – Astoria', direction: 'north' },
   'MTA_550572': { name: 'Q18 – Maspeth', direction: 'north' },
   'MTA_551974': { name: 'Q67 – Court Sq', direction: 'south' },
   'MTA_551950': { name: 'Q67 – Ridgewood', direction: 'south' },
